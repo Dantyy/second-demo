2f7f157f8504af57c2cb920ade1729630f674298
danty 肖 <daxiao@dantydeMacBook-Pro.local>
2020-12-01 11:52:01 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
 // 暂时无法请求api
 import axios from 'axios'
@@ -19,13 +25,25 @@ axios.interceptors.request.use(config => {
   // 为请求头对象，添加Token验证的Authorization字段
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 在最后必须return config
-  console.log(config.headers.Authorization)
+  // console.log(config.headers.Authorization)
   return config
 })
 
 Vue.config.productionTip = false
-
+// 将富文本编辑器，注册为全局可用组件
+Vue.use(VueQuillEditor)
 Vue.component('tree-table', TreeTable)
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 Vue.use(less)
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -31,7 +31,12 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Step,
+  Steps,
+  Checkbox,
+  CheckboxGroup,
+  Upload
 } from 'element-ui'
 
 Vue.use(Button)
@@ -66,3 +71,8 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(TabPane)
 Vue.use(Tabs)
+Vue.use(Step)
+Vue.use(Steps)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
